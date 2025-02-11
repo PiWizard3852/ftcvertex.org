36a69b940b802d44be7aba9fad0a4b479ca16e30
Owen Wood <owenwood0101@icloud.com>
2025-02-11 01:04:01 -0500

    feat(contact, matches): fix navigation

--- src/routes/contact/index.tsx
+++ src/routes/contact/index.tsx
@@ -1,10 +1,16 @@
-import { component$ } from "@builder.io/qwik";
-import { RequestHandler } from "@builder.io/qwik-city";
+import { component$, useTask$ } from "@builder.io/qwik";
+import { RequestHandler, useNavigate } from "@builder.io/qwik-city";
 
 export const onGet: RequestHandler = async ({ redirect }) => {
     throw redirect(301, "mailto:vertex15534@gmail.com");
 };
 
 export default component$(() => {
+    const navigate = useNavigate();
+
+    useTask$(async () => {
+        await navigate("mailto:vertex15534@gmail.com");
+    })
+
     return <div></div>;
 })
